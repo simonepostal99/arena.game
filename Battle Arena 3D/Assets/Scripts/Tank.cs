22b6b783229fd6d6c24b81a27d906872f235826f
simonepostal99 <62949154+simonepostal99@users.noreply.github.com>
2020-03-31 18:50:53 +0200

    Add unity project fix

---
+++ Battle Arena 3D/Assets/Scripts/Tank.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : Unit {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0) && !Input.GetMouseButton(1))
+            Attack();
+
+        if (Input.GetMouseButton(1) && !Input.GetMouseButtonDown(0))
+            Defend();
+    }
+
+    public override void Attack()
+    {
+        Debug.Log("Attacco tank");
+    }
+
+    private void Defend()
+    {
+        Debug.Log("Paro");
+    }
+}
---
+++ Battle Arena 3D/Assets/Scripts/Unit.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Unit : MonoBehaviour{
+
+    [SerializeField] private string unitName;
+    [SerializeField] private UnitType unitType;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float health;
+    [SerializeField] private int level; 
+
+    public string GetUnitName()
+    {
+        return unitName;
+    }
+
+    public UnitType GetUnitType()
+    {
+        return unitType;
+    }
+
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+
+    public float GetHealth()
+    {
+        return health;
+    }
+
+    public int GetLevel()
+    {
+        return level;
+    }
+
+    public abstract void Attack();
+
+}
+
+public enum UnitType
+{
+    tank,
+    range,
+    wizard,
+    monster
+}
