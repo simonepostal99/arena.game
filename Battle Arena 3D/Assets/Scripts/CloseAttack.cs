8be7c22acd9848732cf24855e8e50cd42231ef19
Dimitri Masella <Dimi2000@users.noreply.github.com>
2020-04-03 22:25:57 +0200

    Knockback; CloseAttack; Add player2 test
    knockback nell'attacco;
    Attacco corpo a corpo;
    Aggiunto il player2 per test (copia player1 senza main camera)

---
+++ Battle Arena 3D/Assets/Scripts/CloseAttack.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseAttack : MonoBehaviour
+{
+    //[SerializeField] GameObject cam;
+    [SerializeField] float interactionDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkCollision();
+    }
+
+    private void checkCollision()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            RaycastHit hit;
+            //Ray pointer = new Ray(transform.position, Vector3.forward);
+
+            Debug.DrawRay(transform.position, Vector3.forward * interactionDistance);
+
+
+            if (Physics.Raycast(transform.position, transform.forward, out hit, interactionDistance))
+            {
+                if (hit.collider.tag == "Unit")
+                {
+                    Debug.Log("Colpito!");
+                    hit.collider.GetComponent<Knockback>().knockback(transform.forward);
+                }
+            }
+        }
+    }
+}
---
+++ Battle Arena 3D/Assets/Scripts/Knockback.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour
+{
+    private Rigidbody body;
+    [SerializeField] float backMovement;
+    [SerializeField] float upMovement;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    public void knockback(Vector3 fw)
+    {
+        fw.y = fw.y + upMovement;
+        body.AddForce(fw * backMovement, ForceMode.Impulse);
+    }
+}
